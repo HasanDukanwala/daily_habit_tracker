d8e6d0dd561008276308cf0cbd2aa88c0d0b1f81
HasanDukanwala <dukanwalahasan@gmail.com>
2025-08-23 23:19:02 -0400

    Change the placement of the import to avoid circular imports

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from Flask import Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+# Import models after initializing db to avoid circular imports
+from models import User
 @app.route('/')
 def home():
     return "Welcome to the Daily Habit Tracker!"
