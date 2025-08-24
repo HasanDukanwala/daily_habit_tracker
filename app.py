80bc20eacfc583d10de73e2ebfca54bd300c3821
HasanDukanwala <dukanwalahasan@gmail.com>
2025-08-23 23:08:05 -0400

    Wrote basic setup code to get app running

---
+++ app.py
@@ -0,0 +1,15 @@
+from Flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the Daily Habit Tracker!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
