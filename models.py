66aa95bb082775bb8baccca955b1bea389f51de8
HasanDukanwala <dukanwalahasan@gmail.com>
2025-08-27 19:41:52 -0400

    Created habit class for habit table in DB

--- models.py
+++ models.py
@@ -1,6 +1,22 @@
 from database import db
+from flask_login import UserMixin
+from datetime import datetime
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+    # Relationship to link user to their habits
+    habits = db.relationship('Habit', backref='user', lazy=True)
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    frequency = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(datetime.timezone.utc))
+
+    # Foreign key to link habit to a user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
